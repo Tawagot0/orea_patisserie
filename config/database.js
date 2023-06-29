3bf47149dd42dd9abd14f454a7dc7e3c9632693b
CharlyRc <ricoul.charly@orange.fr>
2023-06-29 15:38:39 +0000

    ra0

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ import mysql from "mysql";
 
 export let pool  = mysql.createPool({
   connectionLimit : 10000,
-    host: "https://149.100.157.233/phpmyadmin/",// on rentre l'hôte, l'adresse url où se trouve la bdd
+    host: "http://149.100.157.233/phpmyadmin/",// on rentre l'hôte, l'adresse url où se trouve la bdd
     user: "root", // identifiant BDD
     password: "Soudan44", // le password
     database: "charlyricoul_projet", // nom de la base de donnée
