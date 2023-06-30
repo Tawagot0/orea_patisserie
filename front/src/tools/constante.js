f66edccfa23565d9ffc7bff920e814f4765577a0
CharlyRc <ricoul.charly@orange.fr>
2023-06-30 21:03:53 +0000

    frz

--- front/src/tools/constante.js
+++ front/src/tools/constante.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://149.100.157.233:8800'
-export const BASE_IMG = 'https://149.100.157.233:8800/img'
+export const BASE_URL = 'http://149.100.157.233:8800'
+export const BASE_IMG = 'http://149.100.157.233:8800/img'
 // export const BASE_URL = 'http://charlyricoul.ide.3wa.io:3001'
 // export const BASE_IMG = 'http://charlyricoul.ide.3wa.io:3001/img'
