4f1cf3d852d0ed6ea5ce54407cf062c825bd9f8a
CharlyRc <ricoul.charly@orange.fr>
2023-07-03 16:26:42 +0000

    maj

--- config/database.js
+++ config/database.js
@@ -2,9 +2,9 @@ import mysql from "mysql";
 
 export let pool  = mysql.createPool({
   connectionLimit : 10000,
-    host: "https://oreapatisserie.online/phpmyadmin",// on rentre l'hôte, l'adresse url où se trouve la bdd
-    user: "root", // identifiant BDD
-    password: "Soudan44", // le password
+    host: "db.3wa.io",// on rentre l'hôte, l'adresse url où se trouve la bdd
+    user: "charlyricoul", // identifiant BDD
+    password: "a7692211db8d28d9016f6ce64f850df2", // le password
     database: "charlyricoul_projet", // nom de la base de donnée
 });
 
--- model/BDD.js
+++ model/BDD.js
@@ -4,9 +4,9 @@ class BDD {
     constructor(){
         this.pool = mysql.createPool({
             connectionLimit : 10000,
-            host: "https://oreapatisserie.online/phpmyadmin",// on rentre l'hôte, l'adresse url où se trouve la bdd
-            user: "root", // identifiant BDD
-            password: "Soudan44", // le password
+            host: "db.3wa.io",// on rentre l'hôte, l'adresse url où se trouve la bdd
+            user: "charlyricoul", // identifiant BDD
+            password: "a7692211db8d28d9016f6ce64f850df2", // le password
             database: "charlyricoul_projet", // nom de la base de donnée
         });
     }
